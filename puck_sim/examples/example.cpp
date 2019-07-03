da36dc90442c5c0da46b15c926f0b6b5bb696ae2
Huong Do Van <33709616+dovanhuong@users.noreply.github.com>
2019-07-03 16:20:05 +0900

    Update example.cpp

--- puck_sim/examples/example.cpp
+++ puck_sim/examples/example.cpp
@@ -1,25 +1,4 @@
-/******************************************************************************************
-*                                                                                        *
-*    Optimal Path Planner to Separate Different-colored Beans with Two Agents            *
-*    Version 1.0                                                                         *
-*    ----------------------------------------------------------                          *
-*    Copyright (C) 2012  Soonkyum Kim                                                    *
-*                                                                                        *
-*    This program is free software: you can redistribute it and/or modify                *
-*    it under the terms of the GNU General Public License as published by                *
-*    the Free Software Foundation, either version 3 of the License, or                   *
-*    (at your option) any later version.                                                 *
-*                                                                                        *
-*    This program is distributed in the hope that it will be useful,                     *
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of                      *
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                       *
-*    GNU General Public License for more details <http://www.gnu.org/licenses/>.         *
-*                                                                                        *
-*                                                                                        *
-*    Contact: kim.soonkyum@gmail.com                                                     *
-*                                                                                        *
-*                                                                                        *
-******************************************************************************************/
+
 #include "ros/ros.h"
 #include "puck_sim/mySim.h"
 
